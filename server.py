29826409b15a08dfb18c50a9a76d06df669fab62
Vincent TEMPLIER <vincent.templier.05@gmail.com>
2024-05-26 10:34:59 +0200

    Add mistral comment app in server.py

--- server.py
+++ server.py
@@ -7,13 +7,42 @@ from main import main
 st.set_page_config(layout="wide")
 is_playing = False
 
-st.title('MistralMatch : Ai footbal comentator')
+#####################################
+# Mistral model setup               #
+#####################################
+
+import os
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+
+api_key = "MnOJOWe78ITuMe9VXRxhBTSnysQT9JvR"
+model = "mistral-large-latest"
+
+client = MistralClient(api_key=api_key)
+
+whole_chat_content = []
+
+initial_prompt = f"""
+Context information is below.
+---------------------
+You are a very expressive and passionate soccer commentator. 
+Each time I will give you a grid with players and the ball location.
+You are supposed to comment on what's happening.
+Here is the soccer legends: G is the goal, A is player on team 1, B is the player on team 2, O the ball and before each letter A or B there is the player number
+---------------------
+Here the map of the game:
+"""
+
+#####################################
+
+st.title('MistralMatch : Ai football comentator')
 GRIDS = []
+
+# User input to play his/her soccer video
+url = st.text_input("Youtube match url")
 
 # Create two columns
 col1, col2 = st.columns(2)
-
-url = st.text_input("Youtube match url")
 
 
 # Column 1 for the video
@@ -46,3 +75,53 @@ with col1:
         media.Player(url=url, controls=True,muted=True,playing=is_playing)
 
 # Column 2 for the streaming text
+# Column 2 for the streaming text
+with col2:
+    st.header("Game comments")
+
+    container = st.container(height=300)
+
+    def stream_game():
+        for word in comment_content.split(" "):
+            yield word + " "
+            time.sleep(0.1)
+
+    # Here generate the map
+    # To change with Json files
+    game_map = f"""
+    G    12A   23B     34B      G
+    G    2B O  15A              G
+    G      5B                   G
+    """
+
+    # Ask the bot to generate the comments
+    whole_chat_content = initial_prompt + game_map
+    chat_response = client.chat(
+        model=model,
+        messages=[ChatMessage(role="user", content=whole_chat_content)]
+    )
+    comment_content = chat_response.choices[0].message.content
+
+    container.write_stream(stream_game)
+
+    whole_chat_content += comment_content
+
+    game_map = f"""
+    G    12A   23B     34B      G
+    G    2B   15A              G
+    G      5BO                   G
+    """
+
+    whole_chat_content += game_map
+
+    # Ask the bot to generate the comments
+    whole_chat_content = initial_prompt + game_map
+    chat_response = client.chat(
+        model=model,
+        messages=[ChatMessage(role="user", content=whole_chat_content)]
+    )
+    comment_content = chat_response.choices[0].message.content
+
+    container.write_stream(stream_game)
+
+    
