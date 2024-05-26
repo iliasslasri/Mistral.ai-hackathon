c9520bed6b8e31b581bb87678286ded34604d8c9
Vincent TEMPLIER <vincent.templier.05@gmail.com>
2024-05-26 11:24:30 +0200

    Delete unused files

--- yolo_inference.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO 
-
-model = YOLO('./models/best.pt')
-
-results = model.predict('./input_videos/match.mp4',save=True)
-print(results[0])
-
-for box in results[0].boxes:
-    print(box)
